b355ac54140b3fa2d3a83d0f8fd25031108530e3
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-08-08 20:45:56 +0300

    Update ProductManager.java

--- src/main/java/productManager/ProductManager.java
+++ src/main/java/productManager/ProductManager.java
@@ -4,40 +4,41 @@ import productRepository.ProductRepository;
 import products.Product;
 
 public class ProductManager {
-    private ProductRepository repository;
+    private ProductRepository repo;
 
-    public ProductManager(ProductRepository repository) {
-        this.repository = repository;
+    public ProductManager(ProductRepository repo) {
+        this.repo = repo;
     }
 
-    public void addProduct(Product product) {
-        repository.save(product);
+    public void add(Product product) {
+        repo.save(product);
     }
 
-    public void removeById (int id) {
-        repository.removeById(id);
-    }
-    public Product[] getProducts() {
-        return repository.getProducts();
-    };
     public Product[] searchBy(String text) {
-        Product[] result = new Product [0];
-        for (Product product: repository.getProducts()) {
+        Product[] result = new Product[0]; // тут будем хранить подошедшие запросу продукты
+        Product[] tmp = new Product[0];
+        for (Product product : repo.getProducts()) {
             if (matches(product, text)) {
-                Product[] tmp = new Product[result.length + 1];
-                for (int i = 0; i < result.length; i++) {
-                    tmp[i] = result[i];
-                }
+                tmp = Arrays.copyOf(result, result.length + 1);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
-
             }
-
         }
         return result;
     }
 
+    // метод определения соответствия товара product запросу search
     public boolean matches(Product product, String search) {
-        return product.getName().contains(search);
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+        // или в одну строку:
+        // return product.getName().contains(search);
+    }
+
+    public Product[] getRepo(){
+        return repo.getProducts();
     }
 }
