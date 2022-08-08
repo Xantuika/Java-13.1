48f2c8073ece64b3a7685df53c87a4afb4cf6a63
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-08-08 20:43:29 +0300

    Update Book.java

--- src/main/java/products/Book.java
+++ src/main/java/products/Book.java
@@ -7,16 +7,15 @@ public class Book extends Product {
         this.author = author;
     }
 
-    //public String getAuthor() {
-       // return author;
-   // }
+    public Book() {
 
-   // public void setAuthor(String author) {
-       // this.author = author;
-   // }
+    }
 
-   // @Override
-       // public void use() {
-          //  System.out.println("Читаем книгу " + name);
-    //}
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
