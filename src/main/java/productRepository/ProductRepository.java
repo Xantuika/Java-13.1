8c23fa42807777531d5e12dd26cd9559c66b5668
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-08-08 20:47:04 +0300

    Update ProductRepository.java

--- src/main/java/productRepository/ProductRepository.java
+++ src/main/java/productRepository/ProductRepository.java
@@ -6,51 +6,27 @@ public class ProductRepository {
     private Product[] products = new Product[0];
 
     public void save(Product product) {
-
-       Product newProduct = findById(product.getId());
-        if (newProduct != null) {
-            throw new AlreadyExistsException(
-                    "Element with id: " + product.getId() + " already exists"
-            );
-        }
-
-    Product[] tmp = new Product[products.length + 1];
+        Product[] tmp = new Product[products.length + 1];
         for (int i = 0; i < products.length; i++) {
             tmp[i] = products[i];
         }
-
         tmp[tmp.length - 1] = product;
-        products= tmp;
+        products = tmp;
     }
 
-    public void removeById(int id) { 
-        Product verifyId = findById (id);
-        if (verifyId == null) {
-            throw new NotFoundException( "Элемент с таким: " + id + " не найден" );
-        }
-        Product [] tmp = new Product[products.length -1];
+    public void removeById(int id) {
+        Product[] tmp = new Product[products.length - 1];
         int copyToIndex = 0;
         for (Product product : products) {
             if (product.getId() != id) {
-                tmp [copyToIndex] = product;
-                copyToIndex ++;
+                tmp[copyToIndex] = product;
+                copyToIndex++;
             }
         }
         products = tmp;
     }
 
-    private Product findById(int id) {
-        for (Product product : products) {
-            if (product.getId() == id) {
-                return product;
-            }
-        }
-        return null;
-    }
-
     public Product[] getProducts() {
         return products;
     }
-
-
 }
